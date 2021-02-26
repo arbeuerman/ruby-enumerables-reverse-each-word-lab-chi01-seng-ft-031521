43b28ebc27f5d552551ad7a498aa0b8de6430ab1
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-26 20:38:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,9 @@ def reverse_each_word(sentence)
   words = sentence.split(" ")
   reversed_sentence = []
   words.each do |word|
-    word.reverse_each #do |letters|
-      #puts letters 
-    #end 
+    word.each do |letters|
+      puts letters 
+    end 
   end 
 end 
 
